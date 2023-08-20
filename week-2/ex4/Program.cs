ebc3d7ba7d78102b1ba3940727ea17a3092cb420
kannikar1306 <142704904+kannikar1306@users.noreply.github.com>
2023-08-20 18:48:15 +0700

    first commit

---
+++ week-2/ex4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+        int input1=30;
+        int input2=6;
+        int total = input1/input2;
+      
+      Console.WriteLine("addition= "); 
+      Console.WriteLine(total);  
+      //Console.WriteLine("Hello World!");    
+    }
+  }
+} 
