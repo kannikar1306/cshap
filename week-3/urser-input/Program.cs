14e1cfb34882d374c3439d593c54535f8fe26f9f
kannikar1306 <142704904+kannikar1306@users.noreply.github.com>
2023-08-27 17:32:14 +0700

    Add files via upload

---
+++ week-3/urser-input/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string fname = "";
+double height = 0;
+double weight = 0.0d;
+double BMI = 0.0d;
+
+Console.Write("input first name : ");    //display ชื่อ
+fname = Console.ReadLine();              //input keyboard
+
+Console.Write("input height : ");         //display ความสูง
+height = double.Parse(Console.ReadLine());  //input keyboard and convert
+
+Console.Write("input weight : ");         //display นน
+weight = double.Parse(Console.ReadLine());  //input keyboard and convert
+
+BMI = height/weight;
+
+BMI = Math.Pow(BMI, 2);
+
+Console.WriteLine("BMI is : "+BMI);
---
+++ week-3/variables/Program.cs
@@ -0,0 +1,48 @@
+﻿/**string ข้อความ ***/
+/*
+string prefix="Mis ";
+string fname = "kannikar ";
+string lname = " patposri";
+/*
+Console.WriteLine(fname);
+Console.WriteLine(lname);
+
+Console.WriteLine(prefix+""+fname + " " + lname);
+*/
+
+
+/*** int ตัวเลข ***/
+/*const int myNum;=20;
+myNum =5;//(15+1+2+4)/4;//calcalate
+
+const double newnum = 5.5D;
+folt   fnumber =5.5;
+
+Console.WriteLine(mynume);
+//Console.WriteLine(fnumber);*/
+
+
+/*** const การประกาศ constance ***/
+/*
+const double myNum=3.14;
+//myNum = 20.00; // error
+ Console.WriteLine(mynum);
+      
+ 
+
+/*** Multiple variables ***/
+//old singie variable
+
+/*int input1 =1;
+int input2 =2;
+int input3 =3;
+*/
+//Multi variable
+int input1=1, input2=2;
+double input3=3.14d, input4=1.22d;
+float f1= 3.33f;
+string str= "Mr. ";
+char ch ='c';
+
+
+Console. WriteLine(str+" "+ch+" "+input1+" "+input3);
